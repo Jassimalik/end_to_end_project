a7160e27baa2683237dd157573cb16217fedfa4d
Jassimalik <am1210797@iitd.ac.in>
2025-02-18 15:03:40 +0530

    Data Ingestion is completed

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
-def sum(a,b):
-    return a+b
+import sys
+import os
+sys.path.append(os.path.abspath("src"))  # Ensure Python finds 'src'
+from mlproject.logger import logging
+from mlproject.exception import CustomException
+from mlproject.components.data_ingestion import DataIngestion
+from mlproject.components.data_ingestion import DataIngestionConfig
+
+if __name__=="__main__":
+    logging.info("The execution start")
+
+    try:
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+    except Exception as ex:
+        raise CustomException(ex,sys)
